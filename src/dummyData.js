6affe34136dc07e9cf19b7a299c90bf6130b06a0
Hammad Ali <hammadalipbt18@gmail.com>
2025-03-13 16:30:04 +0530

    Update dummyData.js cropped sponsors

--- src/dummyData.js
+++ src/dummyData.js
@@ -8,8 +8,8 @@ export const memoriesImages = [
 
 export const sponsors = [
     "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741863128/Untitled_design__3_-removebg-preview_zgzjds.png",
-    "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741863127/Untitled_design__2_-removebg-preview_fxu54h.png",
-    "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741863127/Untitled_design__1_-removebg-preview_lbbwst.png",
+    "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741863547/Untitled_design__2_-removebg-preview_cropped_v29hzd.png",
+    "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741863547/Untitled_design__1_-removebg-preview_cropped_ydgoio.png",
     "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741862731/Untitled_design_vbaqev.png",
     "https://res.cloudinary.com/dzlrsrr11/image/upload/v1741862732/Nasheedio_Logo-02_hknxto.png"
     
